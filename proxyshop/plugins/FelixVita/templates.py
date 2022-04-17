edb50da18af3227b8b60ce30a5cffc4456631bde
HelixVita <mycosynth@protonmail.com>
2022-04-17 11:27:47 +0200

    Handle sets with set symbols lacking outer stroke

--- proxyshop/plugins/FelixVita/templates.py
+++ proxyshop/plugins/FelixVita/templates.py
@@ -55,6 +55,18 @@ sets_with_hollow_set_symbol = [
     # POR
 ]
 
+sets_with_set_symbol_lacking_outer_stroke = [
+    # TODO: Test all of these
+    "VIS",
+    "ARN", # Probably
+    "LEG", # Probably
+    "POR",
+    "ICE",
+]
+
+# Overwrite constants
+con.set_symbol_library["ICE"] = ""  # Use ss-ice2 (instead of ss-ice)
+
 class RetroExpansionSymbolField (txt_layers.TextField):
     """
      * Created by FelixVita
@@ -64,14 +76,16 @@ class RetroExpansionSymbolField (txt_layers.TextField):
      * `rarity`: The clipping mask to enable (uncommon, rare, mythic)
      * `reference`: Reference layer to scale and center
      * `centered`: Whether to center horizontally, ex: Ixalan
+     * `symbol_stroke_size`: The symbol stroke size (thickness).
     """
-    def __init__ (self, layer, text_contents, rarity, reference, centered=False, is_pre_exodus=None, has_hollow_set_symbol=None):
+    def __init__ (self, layer, text_contents, rarity, reference, centered=False, is_pre_exodus=None, has_hollow_set_symbol=None, setcode=None):
         super().__init__(layer, text_contents, psd.rgb_black())
         self.centered = centered
         self.rarity = rarity
         self.reference = reference
         self.is_pre_exodus = is_pre_exodus
         self.has_hollow_set_symbol = has_hollow_set_symbol
+        self.setcode = setcode
         if rarity in (con.rarity_bonus, con.rarity_special):
             self.rarity = con.rarity_mythic
 
@@ -84,12 +98,23 @@ class RetroExpansionSymbolField (txt_layers.TextField):
             else: psd.frame_expansion_symbol(self.layer, self.reference, False)
         app.activeDocument.activeLayer = self.layer
 
-        # Rarity above common?
-        if self.rarity == con.rarity_common or self.is_pre_exodus: psd.apply_stroke(cfg.cfg.symbol_stroke, psd.rgb_white())
+        # Symbol stroke size (thickness)
+        if self.setcode is None: symbol_stroke_size = cfg.cfg.symbol_stroke
+        elif self.setcode in ["ICE", "DRK"]: symbol_stroke_size = 1
+        elif self.setcode in sets_with_set_symbol_lacking_outer_stroke: symbol_stroke_size = 1
+
+        # Symbol stroke color
+
+        # symbol_stroke_color = psd.rgb_black if self.symbol_stroke_size <= 1 else psd.rgb_white
+
+        # Symbol color
+        if self.setcode in ["ICE"]: psd.apply_stroke(symbol_stroke_size, psd.rgb_white())
+        elif self.setcode in sets_with_set_symbol_lacking_outer_stroke: psd.apply_stroke(symbol_stroke_size, psd.rgb_black())
+        elif self.rarity == con.rarity_common or self.is_pre_exodus: psd.apply_stroke(symbol_stroke_size, psd.rgb_white())
         else:
             mask_layer = psd.getLayer(self.rarity, self.layer.parent)
             mask_layer.visible = True
-            psd.apply_stroke(cfg.cfg.symbol_stroke, psd.rgb_white())
+            psd.apply_stroke(symbol_stroke_size, psd.rgb_white())
             psd.select_layer_pixels(self.layer)
             app.activeDocument.activeLayer = mask_layer
             psd.align_horizontal()
@@ -120,14 +145,14 @@ class RetroNinetysevenTemplate (temp.NormalClassicTemplate):
         #     else: psd.getLayer("<layer name>", "<layer group>").visible = False
         super().__init__(layout, file)
         # Overwrite the expansion symbol field text layer using custom class
-        card_set = layout.set.upper()
+        setcode = layout.set.upper()
         text_and_icons = psd.getLayerSet(con.layers['TEXT_AND_ICONS'])
         expansion_symbol = psd.getLayer(con.layers['EXPANSION_SYMBOL'], text_and_icons)
         try: expansion_reference = psd.getLayer(con.layers['EXPANSION_REFERENCE'], text_and_icons)
         except: expansion_reference = None
         for i, layer in enumerate(self.tx_layers):
             if isinstance(layer, txt_layers.ExpansionSymbolField):
-                if card_set in sets_without_set_symbol:
+                if setcode in sets_without_set_symbol:
                     del self.tx_layers[i]
                 else:
                     self.tx_layers[i] = RetroExpansionSymbolField(
@@ -135,8 +160,9 @@ class RetroNinetysevenTemplate (temp.NormalClassicTemplate):
                         text_contents = self.symbol_char,
                         rarity = self.layout.rarity,
                         reference = expansion_reference,
-                        is_pre_exodus = card_set in pre_exodus_sets,
-                        has_hollow_set_symbol = card_set in sets_with_hollow_set_symbol
+                        is_pre_exodus = setcode in pre_exodus_sets,
+                        has_hollow_set_symbol = setcode in sets_with_hollow_set_symbol,
+                        setcode = setcode
                     )
 
     def enable_frame_layers (self):
