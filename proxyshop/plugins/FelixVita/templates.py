44fd7c26dcc462059454709c27167afcbb11dd98
HelixVita <mycosynth@protonmail.com>
2022-08-25 20:32:27 +0200

    Add misc custom constants

--- proxyshop/plugins/FelixVita/templates.py
+++ proxyshop/plugins/FelixVita/templates.py
@@ -8,6 +8,106 @@ import proxyshop.helpers as psd
 import photoshop.api as ps
 app = ps.Application()
 
+list_of_all_mtg_sets = list(con.set_symbols.keys())
+
+pre_modern_sets = list_of_all_mtg_sets[:list_of_all_mtg_sets.index("8ED")]
+
+pre_mmq_sets = list_of_all_mtg_sets[:list_of_all_mtg_sets.index("MMQ")]
+# Mercadian Masques changed the color of the copyright/collector's info on red cards from black to white (though strangely HML had a mix of black & white).
+
+pre_exodus_sets = list_of_all_mtg_sets[:list_of_all_mtg_sets.index("EXO")]
+# Exodus featured a couple of important and lasting changes:
+# 1. Colored set symbols for uncommons and rares (silver & gold color)
+# 2. Artist+Collector text field at the bottom of the card is now centered (previously left-justified)
+
+pre_mirage_sets = list_of_all_mtg_sets[:list_of_all_mtg_sets.index("MIR")]
+# Mirage featured some changes to the frame, including but not limited to:
+# 1. Citations in flavor text are now right-justified
+# 2. Frame of black cards is now darker
+# 3. Wider rules box, with some additional changes for each color:
+# 3.1. W: Backgd now less patterned (less contrast); bevel shadows inverted.
+# 3.2. U: Backgd now less patterned (less contrast); bevel shadows changed.
+# 3.2. B: Parchment no longer surrounded by black box
+# 3.2. R: Bevel shadow intensity slightly changed.
+# 3.2. G: 'Parchment' brighter and less patterned
+# 3.2. M: (No significant changes besides width.)
+# 3.2. A: Bevel shadow width & intensity decreased.
+
+pre_hml_sets = list_of_all_mtg_sets[:list_of_all_mtg_sets.index("HML")]
+# Homelands changed the color of the copyright/collector's info on blue cards from black to white
+
+pre_fourth_sets = list_of_all_mtg_sets[:list_of_all_mtg_sets.index("4ED")]
+# 4ED Made the wooden rules text box of green cards considerably brighter.
+
+pre_legends_sets = list_of_all_mtg_sets[:list_of_all_mtg_sets.index("LEG")]
+# Legends made white text (cardname, typeline, P/T) white instead of gray
+
+pre_atq_sets = list_of_all_mtg_sets[:list_of_all_mtg_sets.index("ATQ")]
+# Antiquities gave white frames a hint of yellow tint
+
+sets_without_set_symbol = [
+    "LEA",
+    "LEB",
+    "2ED",
+    "3ED",
+    "4ED",
+    "5ED",
+]
+
+sets_with_hollow_set_symbol = [
+    "6ED",
+    "DKM",
+    "TOR",
+    "JUD", # Probably
+    "ONS", # Probably
+    # Maybe:
+    # POR
+    "ATQ",
+]
+
+sets_lacking_symbol_stroke = [
+    # The result of putting a set here is that it will get a near-invisible 1-pixel thick BLACK stroke. (Because can't apply 0-px thick stroke.... WAIT A SEC, what about just not applying a stroke at all?)  # TODO: Finish writing this
+    # Only pre-exodus sets should go into this list, as putting a set into this list will result in no gold/silver/mythic color being applied to the set symbol.
+    "VIS",
+    "ARN", # Probably
+    "LEG", # Probably
+    # "POR",
+    "ICE", # Nope, because even a 1px black stroke is too much; it needs to be white.
+    "P02",
+    "ATQ",
+]
+
+sets_with_black_copyright_for_lands = [_ for _ in pre_mmq_sets if _ not in ["USG", "S99"]]
+
+special_land_frames = {
+    "ARN": "Arabian Nights",
+    "ATQ": "Antiquities",
+    "LEG": "Legends",
+    "DRK": "The Dark",
+    "FEM": "Fallen Empires",
+    "ICE": "Ice Age",
+    "HML": "Homelands",
+    "ALL": "Alliances",
+    "MIR": "Mirage",
+    "VIS": "Visions",
+    "4ED": "Fourth Edition",
+}
+
+original_dual_lands = [
+    "Tundra",
+    "Underground Sea",
+    "Badlands",
+    "Taiga",
+    "Savannah",
+    "Scrubland",
+    "Volcanic Island",
+    "Bayou",
+    "Plateau",
+    "Tropical Island",
+]
+
+all_keyrune_pre_eighth_symbols_for_debugging = ""
+
 
 class AncientTemplate (temp.NormalClassicTemplate):
     """
